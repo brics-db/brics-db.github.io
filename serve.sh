cacf4dd30235f00c810aa79c901b03953a9b5841
Till Kolditz <till.kolditz@gmail.com>
2018-04-13 14:41:59 +0200

    add helper script for remebering how to serve jekyll locally
    Signed-off-by: Till Kolditz <till.kolditz@gmail.com>

---
+++ serve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This serves simply as a reminder how to locally serve this jekyll page
+#
+# Also have a look at e.g. https://github.com/pages-themes/minimal
+#
+# use "bundle update" to update the plugins used for this github.io page
+
+bundle exec jekyll serve
+
